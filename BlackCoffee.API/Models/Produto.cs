278e4689e5a1e54ae33037e6653ab20499a95762
lucas souza <lucas2469y@gmail.com>
2025-10-25 13:17:26 -0300

    Dando inicio ao projeto interdeciplinar BlackCoffe

---
+++ BlackCoffee.API/Models/Produto.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FerroFogo.API.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CategoriaId { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "O Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [StringLength(3000)]
+        public string Descricao { get; set; }
+
+        public int Qtde { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal ValorCusto { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        public decimal ValorVenda { get; set; }
+
+        public bool Destaque { get; set; } = false;
+
+        [StringLength(300)]
+        public string Foto { get; set; }
+    }
+}
---
+++ BlackCoffee.API/Models/Usuario.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace cd BlackCoffee.API.Models
+{
+    [Table("Usuario")]
+    public class Usuario : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DataNascimento { get; set; }
+
+        [StringLength(300)]
+        public string? Foto { get; set; }
+    }
+}
